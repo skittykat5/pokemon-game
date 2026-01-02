4da9053e8fdd5d7752b2cbbb975b79b63ec8fcba
skittykat5 <bermudakay@gmail.com>
2026-01-02 08:25:28 -0400

    Added button visual toggle

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,9 +1,17 @@
+import {useState} from 'react';
 function PokeCard({name, imageSrc, isWinner, cardButtonCallback}) {
+
+    const [isSelected, setIsSelected] = useState(false);
+
+    function handleSelect() {
+        setIsSelected(true);
+        cardButtonCallback(isWinner);
+    }
 
     return (
         <div class="pokemon-card">
             <img class="spriteImage" src={imageSrc}/>
-            <button class="button" onClick={() => cardButtonCallback(isWinner)}>
+            <button class={isSelected ? 'button selected' : 'button'} onClick={handleSelect}>
                 {name}
             </button>
         </div>
